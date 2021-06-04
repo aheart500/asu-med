8b87335c4de3c31fac29096b7c171721fa1fdd09
Mohamed Nasser <aheart@500@yahoo.com>
2021-06-05 00:26:44 +0200

    Rank Result FontSize

--- pages/rank/result.tsx
+++ pages/rank/result.tsx
@@ -11,7 +11,7 @@ const useStyle = makeStyles(() => ({
     justifyContent: "center",
     alignItems: "center",
     minHeight: "25rem",
-    fontSize: "11rem",
+    fontSize: "10rem",
   },
 }));
 const ordinal_suffix_of = (i: number) => {
